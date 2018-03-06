8d9db504f10f5edf90d212be74a045191eeff44b
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-03-05 23:14:40 -0500

    commit current progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
-    // Code goes here
+    
+    // Declare global variables
+    var questions = {
+        q1: "In which year was Leonardo da Vinci born?",
+        ans1A: 1567,
+        ans1B: 1452,
+        ans1C: 1499,
+        ans1D: 1525
+    };
+    var answerCorrect = 0;
+    var answerIncorrect = 0;
+
+
+    // Declare functions
+    
+    // Display first question and answers
+
+    
+    // Click on correct/incorrect answer
+
+
+    // Call function to start game
+    
 });
