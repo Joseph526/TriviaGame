b32639d5dd9b164d7c4e0e7090e6262f34da2803
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-03-08 00:30:02 -0500

    added questions, fixed answer click function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
     // Declare global variables
     var questions = [
         {
@@ -31,11 +31,30 @@ $(document).ready(function() {
                 d: "Spain"
             },
             correctAns: "a"
+        },
+        {
+            que: "Which museum is the home of the Mona Lisa?",
+            ans: {
+                a: "Metropolitan Museum of Art",
+                b: "British Museum",
+                c: "Philadelphia Museum of Art",
+                d: "Louvre"
+            },
+            correctAns: "d"
+        },
+        {
+            que: "Who is a famous American painter?",
+            ans: {
+                a: "Paul Cezanne",
+                b: "Andrew Wyeth",
+                c: "Rembrandt",
+                d: "Vincent van Gogh"
+            },
+            correctAns: "b"
         }
     ];
     var intervalId1;
-    var intervalId2;
-    var timeLimit = 5;
+    var timeLimit;
     var i = 0;
     var totalCorrect = 0;
     var totalIncorrect = 0;
@@ -44,42 +63,46 @@ $(document).ready(function() {
 
     // Declare functions
 
-    // Function to start timers and show question
+    // Function to start timer and show question
     function startGame() {
+        timeLimit = 5;
         showQuestion();
-        // intervalId1 = setInterval(showQuestion, 5000);
         // Display initial value of countdown timer
         $("#timerDiv").html("<h4>Time remaining: " + timeLimit + " seconds</h4>");
-        intervalId2 = setInterval(decrement, 1000);
+        intervalId1 = setInterval(decrement, 1000);
     }
-    
+
     // Function to decrement the countdown timer and display on screen
     function decrement() {
         timeLimit--;
         $("#timerDiv").html("<h4>Time remaining: " + timeLimit + " seconds</h4>");
         if (timeLimit === 0) {
-            clearInterval(intervalId2);
+            clearInterval(intervalId1);
             $("#resultDiv").html("<em>You ran out of time!</em>");
             totalUnanswer++;
             i++;
-            timeLimit = 5;
-            if (i < questions.length) {
-                setTimeout(startGame, 5000);    
-            }
-            // Special case to handle last question
-            else if (i === questions.length) {
-                setTimeout(displayFinalScore, 5000);
-            }
+            nextQuestion();
         }
     }
-    
-    // Display question and answers
+
+    // Function to move to next question, including special case for last question
+    function nextQuestion() {
+        if (i < questions.length) {
+            setTimeout(startGame, 5000);
+        }
+        // Special case to handle last question
+        else if (i === questions.length) {
+            setTimeout(displayFinalScore, 5000);
+        }
+    }
+
+    // Function to display question and answers
     function showQuestion() {
         var printQuestion = $("<div>").text(questions[i].que);
-        var printAnsA = $("<div>").text("a: " + questions[i].ans.a).attr("data-ans", "a");
-        var printAnsB = $("<div>").text("b: " + questions[i].ans.b).attr("data-ans", "b");
-        var printAnsC = $("<div>").text("c: " + questions[i].ans.c).attr("data-ans", "c");
-        var printAnsD = $("<div>").text("d: " + questions[i].ans.d).attr("data-ans", "d");
+        var printAnsA = $("<div>").text("a: " + questions[i].ans.a).addClass("answer").attr("data-ans", "a");
+        var printAnsB = $("<div>").text("b: " + questions[i].ans.b).addClass("answer").attr("data-ans", "b");
+        var printAnsC = $("<div>").text("c: " + questions[i].ans.c).addClass("answer").attr("data-ans", "c");
+        var printAnsD = $("<div>").text("d: " + questions[i].ans.d).addClass("answer").attr("data-ans", "d");
         $("#quesDiv").html(printQuestion);
         $("#ansDiv1").html(printAnsA);
         $("#ansDiv2").html(printAnsB);
@@ -87,16 +110,35 @@ $(document).ready(function() {
         $("#ansDiv4").html(printAnsD);
         $("#resultDiv").html("&nbsp");
     }
-    
-    // Click on answer, evaluate if correct/incorrect
+
+    // Listen for click on answer, evaluate if correct/incorrect
     $(document).on("click", ".answer", evaluateAns);
     function evaluateAns() {
         var ansClicked = $(this).attr("data-ans");
-        console.log(ansClicked);
+        if (ansClicked === questions[i].correctAns) {
+            displayCorrect();
+        }
+        else {
+            displayIncorrect();
+        }
     }
 
-    // Display result to user; increment counter
+    // Functions to display correct/incorrect result to user; increment counter
+    function displayCorrect() {
+        clearInterval(intervalId1);
+        $("#resultDiv").html("<em>Correct!</em>");
+        totalCorrect++;
+        i++;
+        nextQuestion();
+    }
 
+    function displayIncorrect() {
+        clearInterval(intervalId1);
+        $("#resultDiv").html("<em>Incorrect!</em>");
+        totalIncorrect++;
+        i++;
+        nextQuestion();
+    }
 
     // Display final scores at game end
     function displayFinalScore() {
@@ -105,7 +147,7 @@ $(document).ready(function() {
         $("#ansDiv2").html("Incorrect answers: " + totalIncorrect);
         $("#ansDiv3").html("Unanswered: " + totalUnanswer);
         $("#ansDiv4").html("&nbsp");
-        $("#resultDiv").html("&nbsp");
+        $("#resultDiv").html("<em>Thanks for playing!</em>");
     }
 
     // Listen for button click to start game
