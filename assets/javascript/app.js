56d4e33b413f1930310c5a525928d90f37adfd41
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-03-07 21:08:46 -0500

    Start Game button working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,71 @@
 $(document).ready(function() {
     
     // Declare global variables
-    var questions = {
-        q1: "In which year was Leonardo da Vinci born?",
-        ans1A: 1567,
-        ans1B: 1452,
-        ans1C: 1499,
-        ans1D: 1525
-    };
-    var answerCorrect = 0;
-    var answerIncorrect = 0;
+    var questions = [
+        {
+            que: "In which year was Leonardo da Vinci born?",
+            ans: {
+                a: "1567",
+                b: "1452",
+                c: "1499",
+                d: "1525"
+            },
+            correctAns: "b"
+        },
+        {
+            que: "Who painted the ceiling of the Sistine Chapel?",
+            ans: {
+                a: "Picasso",
+                b: "Renoir",
+                c: "Michelangelo",
+                d: "Donatello"
+            },
+            correctAns: "c"
+        },
+        {
+            que: "Where did Impressionism originate?",
+            ans: {
+                a: "France",
+                b: "United States",
+                c: "Italy",
+                d: "Spain"
+            },
+            correctAns: "a"
+        }
+    ];
+    var intervalId;
+    var i = 0;
+    var totalCorrect = 0;
+    var totalIncorrect = 0;
 
 
     // Declare functions
-    
-    // Display first question and answers
 
+    // Function to start question timer and call first question
+    function startGame() {
+        intervalId = setInterval(showQuestion, 3000);
+    }
+    
+    // Display question and answers
+    function showQuestion() {
+        var printQuestion = $("<div>").text(questions[i].que);
+        var printAnsA = $("<div>").text("a: " + questions[i].ans.a);
+        var printAnsB = $("<div>").text("b: " + questions[i].ans.b);
+        var printAnsC = $("<div>").text("c: " + questions[i].ans.c);
+        var printAnsD = $("<div>").text("d: " + questions[i].ans.d);
+        $("#quesDiv").html(printQuestion);
+        $("#ansDiv1").html(printAnsA);
+        $("#ansDiv2").html(printAnsB);
+        $("#ansDiv3").html(printAnsC);
+        $("#ansDiv4").html(printAnsD);
+    }
     
     // Click on correct/incorrect answer
 
 
-    // Call function to start game
-    
+    // Display result to user; increment counter
+
+
+    // Listen for button click to start game
+    $(document).on("click", "#startGame", startGame);
 });
